391ff641231599ca9aadc881eb0504928172feb6
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-07-13 02:06:26 +0100

    Search component created

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./style"
+function Search(){
+  return(
+    <S.Search>
+      <input type="text" name="" id="" placeholder="Search here" />
+      <button>Search</button>
+    </S.Search>
+  )
+}
+
+export default Search;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Banner";
 import Header from "../../components/Header";
+import Search from "../../components/Search";
 
 function Home(){
   return(
     <>
       <Header />
       <Banner />
+      <Search />
     </>
   )
 }
